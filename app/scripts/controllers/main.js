df7696bc1a71d5eaf72c1a71e77e74c01b7d1f31
ematav <ematav@icloud.com>
2015-06-02 00:35:47 +0200

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('gestidApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
